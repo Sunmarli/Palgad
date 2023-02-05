b1057d1909f2dc44291be78d3c851b49fbd214d0
Maria Jarv <maria.julia.jarv@gmail.com>
2023-02-05 17:19:14 +0200

    completed

--- spiski/Oma_moodul.py
+++ spiski/Oma_moodul.py
@@ -1,6 +1,6 @@
-from functools import reduce
-from re import I
-
+﻿from collections import Counter
+import heapq
+from math import *
 def loe_failist(file:str)->list:
     fail=open(file,'r',encoding="utf-8-sig")
     mas=[]
@@ -63,3 +63,123 @@ def miinimum(p:list,i:list):
         nimi=i[ind]
         print(f"{nimi}-l on mnimaalne palk")
         pos=ind+1
+
+def sorteeriminekasv(p:list,inim:list):
+    p=list(map(int,p))
+    arg_sort = sorted(range(len(p)), key=lambda i: p[i])
+    
+    print([p[i] for i in arg_sort])
+    print([inim[i] for i in arg_sort])
+
+
+def sorteeriminekah(p:list,inim:list):
+    p=list(map(int,p))
+    #p=(sorted(p, reverse=True))
+    arg_sort = sorted(range(len(p)), key=lambda i: p[i],reverse=True)
+    print([p[i] for i in arg_sort])
+    print([inim[i] for i in arg_sort]) 
+
+def samapalk(p:list,inim:list):
+    p=list(map(int,p))
+    #samad= [a: p.count(a) for a in set(p) if p.count(a) > 1]
+    nimi=[]
+    for a in p:
+        n=p.count(a)
+        pos=0
+        if p.count(a)>1: 
+            for t in range (n):   
+                ind=p.index(a,pos)
+                nim=inim[ind]
+                nimi.append(nim)
+                pos=ind+1
+    print(f"----------{p.count(a)}")
+    print(f"---------{nimi}-l")
+
+def search(p:list,inim:list):
+    n=str(input("Keda otsime?\n "))
+    s=inim.count(n)
+    pos=0
+    for i in inim:
+        if i==n:
+            ind=inim.index(i,pos)
+            pos=ind+1
+            print("Tema palk - ", p[ind])
+
+def suuremkui(p:list,inim:list):
+    n=int(input("Suurem kui ---> "))
+    p=list(map(int,p))
+    pos=0
+    for i in p:
+        if n<=i:
+            ind=p.index(i,pos)
+            pos=ind+1
+            print(f"Suurem kui {n} on --------- {inim[ind]}")
+
+def vaiksemkui(p:list,inim:list):
+    n=int(input("Vaiksem kui ---> "))
+    p=list(map(int,p))
+    pos=0
+    for i in p:
+        if n>=i:
+            ind=p.index(i,pos)
+            pos=ind+1
+            print(f"Vaiksem kui {n} on -------- {inim[ind]}")
+
+def top3(p:list,inim:list):
+    p=list(map(int,p))
+    print("Top3 Suurt palka -- ",sorted(zip(p, inim), reverse=True)[:3])
+    #print(heapq.nlargest(3, zip(p, inim)))
+    print("Top3 Vaiksemat palka -- ",sorted(zip(p, inim), reverse=False)[:3])
+
+def keskmine(p:list,inim:list):
+    p=list(map(int,p))
+    avg = round(sum(p) / len(p))
+    print("Keskmine palk on---", avg)
+    #vah=avg-avg*0.2
+    #vah2=avg+avg*0.2
+    pos=0
+    for i in p:
+        if avg-avg*0.05 <i< avg+avg*0.05:
+            ind=p.index(i,pos)
+            pos=ind+1
+            print(f"Enam vahem keskmine palk on {p[ind]} {inim[ind]}-l") 
+
+def tulumaks(p:list,inim:list):
+    katte=[]
+    p=list(map(int,p))
+    for i in p:
+        if i<654:
+            puh=i
+            katte.append(puh)
+        elif 654 <=i <=1200:
+            puh=654+(i-654)*0.8
+            katte.append(puh)
+        elif  1200<=i<2100:
+            vaba=654-654/900*(i-1200)
+            puh=vaba+(i-vaba)*0.8
+            katte.append(puh)
+        elif i>=2100:
+            puh=round(i*0.8)
+            katte.append(puh) 
+    tulemus=list(zip(katte,inim))
+    #print("Tulumaksuvaba \n",katte) 
+    print(tulemus)
+    
+def sortbyname(p:list,inim:list): 
+    
+    arg_sort = sorted(range(len(inim)), key=lambda i: inim[i])
+    
+    print([p[i] for i in arg_sort])
+    print([inim[i] for i in arg_sort]) 
+
+def deleteloweravg(p:list,inim:list):
+    p=list(map(int,p))
+    avg = round(sum(p) / len(p))
+    #pos=0
+    for i in p:
+        if i<=avg:
+            ind=p.index(i)
+            p.remove(i)
+            inim.pop(ind)
+
+    print(p,inim)
--- spiski/spiski.py
+++ spiski/spiski.py
@@ -2,7 +2,7 @@
 from Oma_moodul import *
 
 while True:
-    print("0-loe failist\n1-andmete lisamine\n2-salvestame failisse\n3-Kustuta\n 4-Maksimaalne palk\n5-Minimaalne  ")
+    print("0-loe failist\n1-andmete lisamine\n2-salvestame failisse\n3-Kustuta\n 4-Maksimaalne palk\n5-Minimaalne\n6 Sorteerimine\n7 Samapalk\n8 Nimijargi otsing\n9Suurem/vaiksem kui xxx\n10 Top3\n11 Keskmine palk\n 12 Tulumalks ")
     v=int(input())
     if v==0:
         palgad=[]
@@ -24,3 +24,28 @@ while True:
         maximumpalk(palgad,inimesed)
     elif v==5:
         miinimum(palgad,inimesed)
+    elif v==6:
+        print("Kasvavalt")
+        sorteeriminekasv(palgad,inimesed)
+        print("Kahanevalt")
+        sorteeriminekah(palgad,inimesed)
+    elif v==7:
+        print(samapalk(palgad,inimesed))
+    elif v==8:
+        search(palgad,inimesed)
+    elif v==9:
+        m=int(input("Suurem kui XX vajuta 1\nVaiksem kui XX vajuta 2\n"))
+        if m==1:
+            suuremkui(palgad,inimesed)
+        if m==2:
+            vaiksemkui(palgad,inimesed)
+    elif v==10:
+        top3(palgad,inimesed)
+    elif v==11:
+        keskmine(palgad,inimesed)
+    elif v==12:
+        tulumaks(palgad,inimesed)
+    elif v==13:
+        sortbyname(palgad,inimesed)
+    elif v==14:
+        deleteloweravg(palgad,inimesed)
